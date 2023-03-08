55bcb13a349b19d136bd99b745b09798530f63f7
sobbey99 <sobbey99@gmail.com>
2023-03-08 11:30:52 +0200

    Try running localhost:8080 and conne cting to MongoDB

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import express from "express"
 import * as dotenv from "dotenv"
 import cors from "cors"
 
+import connectDB from "./mongodb/connect.js";
+
 dotenv.config();
 
 const app = express();
@@ -15,8 +17,12 @@ app.get("/", (req,res) => {
 
 const startServer = async () => {
     try {
-        //connect to the database...
+        connectDB(process.env.MONGODB_URL)
+
+        app.listen(8080, () => console.log("Server started on port http://localhost:8080"))
     } catch (error) {
         console.log(error)
     }
 }
+
+startServer()
